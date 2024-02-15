347bef9bf45e5f34995744f7378ecd07bc1c906a
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2024-02-14 23:40:28 -0700

    Colors

--- music/mobile/lib/main.dart
+++ music/mobile/lib/main.dart
@@ -8,33 +8,58 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const String appTitle = "Music App";
+    final Color backgroundColor = const Color(0xFF6b8094); 
+    final Color textColor = const Color(0xFF191921);
+        final Color textColor2 = const Color(0xFF483D4D);
+
+    final Color navColor = const Color(0xFF556575);
+
     return MaterialApp(
       title: appTitle,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text(appTitle),
+          title:  Text(
+            appTitle,
+            style: TextStyle(color: textColor2),
+            ),
+            backgroundColor: navColor,
         ),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              Text("Welcome to the Crow Moon music app"),
-              Text("Recently visited"),
-              Text("Recently Played"),
-              Text("Currently playing"),
+              Text("Welcome to the Crow Moon music app",style: TextStyle(color: textColor),),
+              Text("Recently visited",style: TextStyle(color: textColor),),
+              Text("Recently Played",style: TextStyle(color: textColor),),
+              
+              Container(
+                width: double.infinity, // Width spans the entire screen
+                color: Colors.brown[300], // Example background color
+                padding: EdgeInsets.all(8.0), // Add padding around the text
+                child: Text(
+                  "Currently playing",
+                  style: TextStyle(color: textColor, fontWeight: FontWeight.bold),
+                ),     
+              ),
               Image.network(
                 'https://www.themilfordmessenger.com/wp-content/uploads/2022/04/mother_mother_inside_artwork.jpg',
                 width: 200,
                 height: 200,
               ),
+
             ],
           ),
         ),
+        backgroundColor: backgroundColor,
         bottomNavigationBar: BottomNavigationBar(
           currentIndex: 1,
+          backgroundColor: navColor,
           items: [
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-            BottomNavigationBarItem(icon: Icon(Icons.home_filled), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.library_music_outlined), label: "Library")
+            BottomNavigationBarItem(icon: Icon(Icons.search, color: textColor2), label: "Search"),
+            BottomNavigationBarItem(icon: Icon(Icons.home_filled,color: textColor2), label: "Home",),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.library_music_outlined, color: textColor2), 
+              label: "Library",
+              )
           ],
         ),
       ),
@@ -58,6 +83,7 @@ class MyApp extends StatelessWidget {
 //#2F3841 black
 //#483D4D bbrowning 
 //#1D2C27 green
+//646D72
 
 //Method that takes the users current time and has it change between 
 //"Good morning" if beforen noon
