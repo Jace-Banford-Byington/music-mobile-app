b4fce1502521953e160e43e2494d3fc773c87ba2
Jace-Banford-Byington <100317211+Jace-Banford-Byington@users.noreply.github.com>
2024-02-27 14:41:47 -0700

    3 screens and it tecnically works

--- music/mobile/lib/common/profile.dart
+++ music/mobile/lib/common/profile.dart
@@ -1,32 +1,39 @@
 import 'package:flutter/material.dart';
 
 class Profile extends StatefulWidget {
-  final bool isChecked;
-
-  const Profile({Key? key, required this.isChecked}) : super(key: key);
+  const Profile({Key? key}) : super(key: key);
 
   @override
   _ProfileState createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
-  bool _isChecked = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _isChecked = widget.isChecked;
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Checkbox(
-      value: _isChecked,
-      onChanged: (newValue) {
-        setState(() {
-          _isChecked = newValue!;
-        });
-      },
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Profile'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.network(
+            'https://picsum.photos/200',
+            width: 100,
+            height: 100,
+          ),
+          SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: TextField(
+              decoration: InputDecoration(
+                labelText: 'Your Username',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
